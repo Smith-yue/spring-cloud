c6ca21288c585d8467c81d8d3616ee49833a1d01
wb-jjb318191 <wb-jjb318191@alibaba-inc.com>
2018-09-12 12:08:43 +0800

    gitignore添加对.iml的排除

--- spring-cloud-config-client/src/main/java/com/bob/springcloud/config/client/SpringCloudConfigClientApplication.java
+++ spring-cloud-config-client/src/main/java/com/bob/springcloud/config/client/SpringCloudConfigClientApplication.java
@@ -2,8 +2,15 @@ package com.bob.springcloud.config.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.client.ConfigClientAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * SpringCloudConfig Client的配置信息都在{@link ConfigClientAutoConfiguration} 里面
+ * 通过 spring.factories文件引入
+ *
+ * @see ConfigClientAutoConfiguration
+ */
 @SpringBootApplication(scanBasePackages = "com.bob.springcloud.config.client.controller")
 public class SpringCloudConfigClientApplication {
 
