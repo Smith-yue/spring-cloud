78b88228d8208b3acaa11c74292e4b1af4b82571
wb-jjb318191 <wb-jjb318191@alibaba-inc.com>
2018-09-17 09:48:51 +0800

    SpringCloudConfig添加bootstrap.yml,转移config配置

--- spring-cloud-config-client/src/main/java/com/bob/springcloud/config/client/SpringCloudConfigClientApplication.java
+++ spring-cloud-config-client/src/main/java/com/bob/springcloud/config/client/SpringCloudConfigClientApplication.java
@@ -2,6 +2,7 @@ package com.bob.springcloud.config.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.bootstrap.BootstrapConfiguration;
 import org.springframework.cloud.config.client.ConfigClientAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -9,8 +10,10 @@ import org.springframework.context.annotation.ComponentScan;
  * SpringCloudConfig Client的配置信息都在{@link ConfigClientAutoConfiguration} 里面
  * 通过 spring.factories文件引入
  *
- * @see ConfigClientAutoConfiguration
+ * @author jjb
+ * @since 2018-09-12
  */
+@BootstrapConfiguration
 @SpringBootApplication(scanBasePackages = "com.bob.springcloud.config.client.controller")
 public class SpringCloudConfigClientApplication {
 
