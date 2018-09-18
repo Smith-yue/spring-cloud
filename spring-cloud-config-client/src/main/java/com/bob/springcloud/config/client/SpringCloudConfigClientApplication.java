1e5f04dbb3592324fb5feb62c7899e05aa0bcbe8
wb-jjb318191 <wb-jjb318191@alibaba-inc.com>
2018-09-18 10:20:54 +0800

    SpringCloudConfig添加bootstrap.yml,转移config配置

--- spring-cloud-config-client/src/main/java/com/bob/springcloud/config/client/SpringCloudConfigClientApplication.java
+++ spring-cloud-config-client/src/main/java/com/bob/springcloud/config/client/SpringCloudConfigClientApplication.java
@@ -4,11 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.bootstrap.BootstrapConfiguration;
 import org.springframework.cloud.config.client.ConfigClientAutoConfiguration;
+import org.springframework.cloud.config.client.ConfigServicePropertySourceLocator;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 /**
  * SpringCloudConfig Client的配置信息都在{@link ConfigClientAutoConfiguration} 里面
  * 通过 spring.factories文件引入
+ *
+ * 默认初始化时通过 {@link ConfigServicePropertySourceLocator#locate(Environment)} 向Server发送请求
+ * 请求发送到
+ * @RequestMapping("/{name}/{profiles}/{label:.*}") {@link EnvironmentController#labelled()} 中
+ *
  *
  * @author jjb
  * @since 2018-09-12
--- spring-cloud-config-server/src/main/java/com/bob/springcloud/config/server/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/bob/springcloud/config/server/SpringCloudConfigServerApplication.java
@@ -2,11 +2,19 @@ package com.bob.springcloud.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.client.ConfigServicePropertySourceLocator;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.config.server.config.ConfigServerProperties;
+import org.springframework.cloud.config.server.environment.EnvironmentController;
+import org.springframework.core.env.Environment;
 
 /**
  * 配置选项：{@link ConfigServerProperties}
+ * 默认情况下Client初始化时通过
+ * {@link ConfigServicePropertySourceLocator#locate(Environment)}
+ * 向Server发送请求
+ *
+ * @RequestMapping("/{name}/{profiles}/{label:.*}") {@link EnvironmentController#labelled(String, String, String)} 中,
  */
 @EnableConfigServer
 @SpringBootApplication
